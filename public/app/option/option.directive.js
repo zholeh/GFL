de7ef8b06f53272cfc5a081f9ae849f3dc6af0af
Oleh Zhaivoronok <zholeg@ukr.net>
2018-12-05 22:04:11 +0200

    recover filters from bag

--- public/app/option/option.directive.js
+++ public/app/option/option.directive.js
@@ -32,55 +32,72 @@
 
   function OptionController($scope) {
     this.$scope = $scope;
-    this.$scope.fields = [
-      { name: "name", description: "Name", type: "input" },
-      { name: "userId.name", description: "User name", type: "input" },
-      { name: "done", description: "Done", type: "checkbox", enabled: false }
+    this.$scope.filters = [
+      { value: "", name: "name", description: "Name", type: "input" },
+      {
+        value: "",
+        name: "userId.name",
+        description: "User name",
+        type: "input"
+      },
+      {
+        value: false,
+        name: "done",
+        description: "Done",
+        type: "checkbox",
+        enabled: false
+      },
+      {
+        value: 0,
+        name: "importance",
+        description: "Importance",
+        type: "slider",
+        enabled: false,
+        min: 0,
+        max: 10
+      }
     ];
 
-    this.$scope.filterTasks = function(field) {
+    this.$scope.filterTasks = function(event) {
       var arrTasks = this.$parent.primaryTasks.concat();
       for (var key in $scope.filters) {
-        var query = $scope.filters[key];
-        if (typeof query === "string") {
+        var field = $scope.filters[key];
+        var query = field.value;
+        if (typeof field.value === "string") {
           query = query.trim();
           if (query === "") {
             continue;
           }
-        } else if (typeof query === "boolean") {
+        } else if (typeof query === "boolean" || typeof query === "number") {
           if (!field.enabled) {
             continue;
           }
         }
-        var arrField = key.split(".");
+        var arrField = field.name.split(".");
         arrTasks = arrTasks.filter(function(el) {
           var val = deepValue(el, arrField);
-          if (typeof query === "boolean") {
+          if (typeof query === "boolean" || typeof query === "number") {
             return query === val;
           } else {
             var reg = new RegExp(query, "i");
-            return val.search(reg) !== -1;
+            return (typeof val === 'string') ? val.search(reg) !== -1 : false;
           }
         });
       }
-      this.$parent.$parent.tasks = arrTasks;
+      $scope.tasks = arrTasks.concat();
     };
 
     this.$scope.sort = function(field, direct) {
       var desc = direct === "desc";
       var arrField = field.split(".");
-      this.$parent.tasks = this.$parent.tasks
-        .map(function(el) {
-          return el;
-        })
-        .sort(function(a, b) {
-          var res;
-          var valA = deepValue(a, arrField);
-          var valB = deepValue(b, arrField);
-          if (desc) res = (valA < valB ? 1 : -1) || 0;
-          else res = (valA > valB ? 1 : -1) || 0;
-          return res;
-        });
+      this.$parent.tasks = this.$parent.tasks.concat().sort(function(a, b) {
+        var res;
+        var valA = deepValue(a, arrField);
+        var valB = deepValue(b, arrField);
+        if (desc) res = (valA < valB ? 1 : -1) || 0;
+        else res = (valA > valB ? 1 : -1) || 0;
+        return res;
+      });
     };
   }
 })();
